120919ab5b21dd606a07b1e07932e65e623553be
pasgeekbrains <pestovartem@mail.ru>
2021-03-04 14:32:29 +0300

    Les 4 v 0.3

--- gb_parse/settings.py
+++ gb_parse/settings.py
@@ -39,10 +39,10 @@ COOKIES_ENABLED = True
 TELNETCONSOLE_ENABLED = False
 
 # Override the default request headers:
-#DEFAULT_REQUEST_HEADERS = {
+DEFAULT_REQUEST_HEADERS = {
    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
    'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3',
-#}
+}
 
 # Enable or disable spider middlewares
 # See https://docs.scrapy.org/en/latest/topics/spider-middleware.html
